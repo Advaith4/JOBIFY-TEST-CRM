fa34b333a5caa77831dd137679df2080eb331804
Advaith4 <advaith.2301006@srec.ac.in>
2026-04-19 10:35:21 +0530

    Remove unused project files

--- src/models/resume.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from datetime import datetime
-from sqlmodel import Field, SQLModel
-
-class Resume(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user.id", index=True)
-    raw_text: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
--- test.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-req = urllib.request.Request('http://127.0.0.1:8000/api/v1/auth/login', data=b'{"username":"advaith","password":"test"}', headers={'Content-Type':'application/json'})
-try:
-    print(urllib.request.urlopen(req).read().decode())
-except Exception as e:
-    try:
-        print(e.read().decode())
-    except:
-        print(e)
